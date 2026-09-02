1a3503134b6a4826be34b62932001f4f51b320cb
laurasouzae <laurasouzae2@gmail.com>
2026-09-02 20:12:42 -0300

    batatinha didadico

--- curso-react/src/pages/Auth.jsx
+++ curso-react/src/pages/Auth.jsx
@@ -1,15 +1,24 @@
+import {useState} from 'react'
 import {Link} from 'react-router'
 function Auth (){
+    const [batatinha, setBatatinha]= useState(0);
+
+    function sub(){
+        setBatatinha(batatinha - 1)
+    }
+
     return(
-        
+    
         
         <div className="mt-16 bg-primary flex flex-col text-white rounded-lg text-center w-1/2 mx-auto py-4 shadow-lg px-4">
             
            
             
             <h2 className="text-white">Login</h2>
-            
-        <form className="flex h-full flex-col  gap-[20]">
+            <div className="bg-red-400 rounded-full p-2" onClick={sub}>-</div>
+            {batatinha} 
+            <div className="bg-blue-400 rounded-full p-2" onClick={()=> setBatatinha (batatinha +1 )} >+</div>
+         <form className="flex h-full flex-col  gap-[20]">
 
            <span className="text-left "> Email:</span> <input className="flex  hover:bg-gray hover:shadow-inner " id="iEmailLogin" type="email" placeholder="Digite seu email: ex: jaoa@gmail.com" /> 
             <span className="text-left ">Senha:</span> <input id="iPassLogin" className=" hover:bg-gray hover:shadow-inner" type="password" placeholder="Letra maiúscula e números" />
